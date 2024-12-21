93fdacfe0e9cfe6991915c56821f31588e605c22
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-12-21 14:30:36 +0530

    Add: Basic Expenses CRUD Endpoints with Fake Data

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+
+import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
+
+// Middleware
+app.use("*", logger());
 
 app.get("/", (c) => {
   return c.json({ message: "Hello from bun" });
 });
+
+app.route("/api/expenses", expensesRoute);
 
 export default app;
